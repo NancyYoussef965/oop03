f53515140deafdb740cc4672106fc46d29ca4323
Nancy <ny6049812@gmail.com>
2025-07-24 13:52:01 +0300

    Q1

--- oop03/Program.cs
+++ oop03/Program.cs
@@ -4,8 +4,46 @@ namespace oop03
 {
     internal class Program
     {
+
+        #region Q1
+        public enum SecurityLevel
+        {
+            Guest,
+            Developer,
+            Secretary,
+            DBA
+        }
+        #endregion
         static void Main(string[] args)
         {
+
+            #region Q1
+            employee emp = new employee()
+            {
+                Id = 101,
+                Name = "nancy youssef",
+                Gender = 'F',
+                Security = SecurityLevel.Developer,
+                Salary = 12000.50m,
+                HireDate = new DateTime(2021, 5, 15)
+
+
+
+            };
+            Console.WriteLine(emp.ToString());
+
+
+
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
         }
     }
 }
---
+++ oop03/employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static oop03.Program;
+
+namespace oop03
+{
+    internal class employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public SecurityLevel Security { get; set; }
+        public DateTime HireDate { get; set; }
+
+        private char gender;
+        private decimal _salary;
+
+        public char Gender
+        {
+            get { return gender; }
+            set
+            {
+                if (value == 'M' || value == 'F')
+                    gender = value;
+                else
+                    throw new ArgumentException("Gender must be 'M' or 'F'");
+            }
+        }
+
+
+        public decimal Salary
+        {
+            get { return _salary; }
+            set
+            {
+                if (value >= 0)
+                    _salary = value;
+                else
+                    throw new ArgumentException("Salary must be non-negative");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}\n" +
+                   $"Name : {Name}\n" +
+                   $"Gender: {Gender}\n" +
+                   $"Security: {Security}\n" +
+                   $"Hire Date: {HireDate.ToShortDateString()}\n" +
+                   $"Salary: {string.Format("{0:C}", Salary)}";
+        }
+    }
+
+ 
+}
